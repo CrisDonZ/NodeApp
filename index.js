cd86a83687b333499b9157884594980616b59843
cried <cried@DESKTOP-EJRMGED>
2025-06-05 22:30:10 -0500

    port from process.env

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log('Server running on http://localhost:3000');
     
 })
